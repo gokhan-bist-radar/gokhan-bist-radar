d8a122a2e27f7b64b70f21992b6af8be57db9dec
gokhan-bist-radar <gokhandelen@gmail.com>
2026-06-12 01:18:12 +0300

    Update gokhan_bist_radar_v13.py

--- gokhan_bist_radar_v13.py
+++ gokhan_bist_radar_v13.py
@@ -1417,9 +1417,9 @@ def main():
             f"RS20 {r.get('rs_xu100_20')} | kırılım {r['breakout_level']} | fib1.618 {r['fib_1618']} | "
             f"R/R {r['risk_reward']} | {r.get('available_tfs','')} | {r['setup']}\n"
         )
-    summary += format_memory_leaders()
-    print("MEMORY SAVE:", len(top))
+    
     save_radar_memory(started, "V13", top)
+    summary += format_memory_leaders()
     telegram_send_message(token, chat_id, summary)
 
     # Grafik gönderimi kapatıldı
